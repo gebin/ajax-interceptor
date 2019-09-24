5ed42e0c85181184f76343da44318e8ad0dce654
bge <bge@che300.com>
2019-09-24 21:49:20 +0800

    重新整理代码结构

--- pageScripts/main.js
+++ pageScripts/main.js
@@ -1,226 +1,277 @@
+// 更新聊天信息
+const POLLING_URLS = [
+    {
+        match: '//im.mafengwo.cn/admin/chat/polling',
+        version: '2.0'
+    }
+]
+// 发送回复消息
+const POST_MESSAGE_URLS = [
+    {
+        match: "//im.mafengwo.cn/admin/chat/message_post",
+        version : "2.0"
+    },
+]
+
+window.userReplyMap = {};
+window.replyTime = 3000;
+window.replyText = '(桃心小蜂)';
+
 // 命名空间
 let ajax_interceptor_qoweifjqon = {
-  settings: {
-    ajaxInterceptor_switchOn: false,
-    ajaxInterceptor_rules: []
-  },
-  originalXHR: window.XMLHttpRequest,
-  myXHR: function() {
-    let pageScriptEventDispatched = false;
-    const xhr = new ajax_interceptor_qoweifjqon.originalXHR();
+    settings: {
+        ajaxInterceptor_switchOn: false,
+        ajaxInterceptor_rules: []
+    },
+    originalXHR: window.XMLHttpRequest,
+    myXHR: function() {
+        let pageScriptEventDispatched = false;
+        const xhr = new ajax_interceptor_qoweifjqon.originalXHR();
 
-    const modifyResponse = () => {
-      // 对比是否满足指定的规则
-      [
-        // {
-        //   match: "https://im.mafengwo.cn/admin/chat/message_post"
-        // },
-        {
-          match: "https://im.mafengwo.cn/admin/chat/polling"
-        }
-      ].forEach(({ match, overrideTxt = "" }) => {
-        if (match && this.responseURL.indexOf(match) > -1) {
-          if (this.response) {
-            // 继续判断是不是符合要求，比如某个字段
-            let responseJSON = JSON.parse(this.response);
-            if(!responseJSON.data.list || responseJSON.data.list.length === 0){
-              return;
-            }
+        const modifyResponse = () => {
+            // 对比是否满足指定的规则
+            POLLING_URLS.forEach(({match, version}) => {
+                if (match && this.responseURL.indexOf(match) > -1) {
+                    if (this.response) {
+                        // 继续判断是不是符合要求，比如某个字段
+                        let responseJSON = JSON.parse(this.response);
+                        handleNewMessage(responseJSON,version)
+                    }
 
-            try{
-              debugger
-              let line_id = responseJSON.data.list[0].item.info.line_id;
-              let name = responseJSON.data.list[0].item.name;
+                    if (!pageScriptEventDispatched) {
+                        window.dispatchEvent(
+                            new CustomEvent('pageScript', {
+                                detail: {url: this.responseURL, match}
+                            })
+                        );
+                        pageScriptEventDispatched = true;
+                    }
+                }
+            });
 
-              // $.ajax({
-              //   url: "https://im.mafengwo.cn/rest/im/event/",
-              //   method: "post",
-              //   data: {
-              //     update: {
-              //       e: "req.message.post",
-              //       b: {
-              //         message: {
-              //           type: 1,
-              //           to_uid: "484173610",
-              //           timestamp: new Date().getTime(),
-              //           content: {
-              //             text: "您好！"
-              //           },
-              //           prefix_id: new Date().getTime()
-              //         },
-              //         role: {
-              //           is_b: "1",
-              //           is_pc: "1"
-              //         },
-              //         t: new Date().getTime(),
-              //         v: "1.0"
-              //       }
-              //     },
-              //     post_style: "default",
-              //     after_style: "default"
-              //   },
-              //   success: function(res) {
-              //     console.log(res);
-              //   },
-              //   error: function() {}
-              // });
-
-              if(name === 'message_new'){
-                setTimeout(function(){
-                  console.log(line_id);
-                  $.ajax({
-                    url: "https://im.mafengwo.cn/admin/chat/message_post",
-                    method: "post",
-                    data: {
-                      line_id:line_id,
-                      message:{
-                        type:1,
-                        content:{
-                          text : '(桃心小蜂)'
-                        }
-                      }
-                    },
-                    success: function(res) {
-                      console.log(res);
-                    },
-                    error: function() {}
-                  });
-                },3000)
-              }
-            }catch(e){
-
-            }
-          }
-
-          if (!pageScriptEventDispatched) {
-            window.dispatchEvent(
-              new CustomEvent("pageScript", {
-                detail: { url: this.responseURL, match }
-              })
-            );
-            pageScriptEventDispatched = true;
-          }
-        }
-      });
-    };
-
-    for (let attr in xhr) {
-      if (attr === "onreadystatechange") {
-        xhr.onreadystatechange = (...args) => {
-          if (this.readyState == 4) {
-            // 请求成功
-            if (ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_switchOn) {
-              // 开启拦截
-              modifyResponse();
-            }
-          }
-          this.onreadystatechange && this.onreadystatechange.apply(this, args);
+            // 判断是否人工回复，如果回复成功，就清空所有的回复。
+            
         };
-        continue;
-      }
 
-      if (typeof xhr[attr] === "function") {
-        this[attr] = xhr[attr].bind(xhr);
-      } else {
-        // responseText和response不是writeable的，但拦截时需要修改它，所以修改就存储在this[`_${attr}`]上
-        if (attr === "responseText" || attr === "response") {
-          Object.defineProperty(this, attr, {
-            get: () =>
-              this[`_${attr}`] == undefined ? xhr[attr] : this[`_${attr}`],
-            set: val => (this[`_${attr}`] = val),
-            enumerable: true
-          });
-        } else {
-          Object.defineProperty(this, attr, {
-            get: () => xhr[attr],
-            set: val => (xhr[attr] = val),
-            enumerable: true
-          });
+        for (let attr in xhr) {
+            if (attr === 'onreadystatechange') {
+                xhr.onreadystatechange = (...args) => {
+                    if (this.readyState == 4) {
+                        // 请求成功
+                        if (ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_switchOn) {
+                            // 开启拦截
+                            modifyResponse();
+                        }
+                    }
+                    this.onreadystatechange && this.onreadystatechange.apply(this, args);
+                };
+                continue;
+            }
+
+            if (typeof xhr[attr] === 'function') {
+                this[attr] = xhr[attr].bind(xhr);
+            } else {
+                // responseText和response不是writeable的，但拦截时需要修改它，所以修改就存储在this[`_${attr}`]上
+                if (attr === 'responseText' || attr === 'response') {
+                    Object.defineProperty(this, attr, {
+                        get: () => (this[`_${attr}`] == undefined ? xhr[attr] : this[`_${attr}`]),
+                        set: val => (this[`_${attr}`] = val),
+                        enumerable: true
+                    });
+                } else {
+                    Object.defineProperty(this, attr, {
+                        get: () => xhr[attr],
+                        set: val => (xhr[attr] = val),
+                        enumerable: true
+                    });
+                }
+            }
         }
-      }
-    }
-  },
+    },
 
-  originalFetch: window.fetch.bind(window),
-  myFetch: function(...args) {
-    return ajax_interceptor_qoweifjqon.originalFetch(...args).then(response => {
-      let txt = undefined;
-      ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_rules.forEach(
-        ({ match, overrideTxt = "" }) => {
-          if (match && response.url.indexOf(match) > -1) {
-            window.dispatchEvent(
-              new CustomEvent("pageScript", {
-                detail: { url: response.url, match }
-              })
+    originalFetch: window.fetch.bind(window),
+    myFetch: function(...args) {
+        return ajax_interceptor_qoweifjqon.originalFetch(...args).then(response => {
+            let txt = undefined;
+            ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_rules.forEach(
+                ({match, overrideTxt = ''}) => {
+                    if (match && response.url.indexOf(match) > -1) {
+                        window.dispatchEvent(
+                            new CustomEvent('pageScript', {
+                                detail: {url: response.url, match}
+                            })
+                        );
+                        txt = overrideTxt;
+                    }
+                }
             );
-            txt = overrideTxt;
-          }
-        }
-      );
 
-      if (txt !== undefined) {
-        const stream = new ReadableStream({
-          start(controller) {
-            const bufView = new Uint8Array(new ArrayBuffer(txt.length));
-            for (var i = 0; i < txt.length; i++) {
-              bufView[i] = txt.charCodeAt(i);
+            if (txt !== undefined) {
+                const stream = new ReadableStream({
+                    start(controller) {
+                        const bufView = new Uint8Array(new ArrayBuffer(txt.length));
+                        for (var i = 0; i < txt.length; i++) {
+                            bufView[i] = txt.charCodeAt(i);
+                        }
+
+                        controller.enqueue(bufView);
+                        controller.close();
+                    }
+                });
+
+                const newResponse = new Response(stream, {
+                    headers: response.headers,
+                    status: response.status,
+                    statusText: response.statusText
+                });
+                const proxy = new Proxy(newResponse, {
+                    get: function(target, name) {
+                        switch (name) {
+                            case 'ok':
+                            case 'redirected':
+                            case 'type':
+                            case 'url':
+                            case 'useFinalURL':
+                            case 'body':
+                            case 'bodyUsed':
+                                return response[name];
+                        }
+                        return target[name];
+                    }
+                });
+
+                for (let key in proxy) {
+                    if (typeof proxy[key] === 'function') {
+                        proxy[key] = proxy[key].bind(newResponse);
+                    }
+                }
+
+                return proxy;
+            } else {
+                return response;
             }
-
-            controller.enqueue(bufView);
-            controller.close();
-          }
         });
-
-        const newResponse = new Response(stream, {
-          headers: response.headers,
-          status: response.status,
-          statusText: response.statusText
-        });
-        const proxy = new Proxy(newResponse, {
-          get: function(target, name) {
-            switch (name) {
-              case "ok":
-              case "redirected":
-              case "type":
-              case "url":
-              case "useFinalURL":
-              case "body":
-              case "bodyUsed":
-                return response[name];
-            }
-            return target[name];
-          }
-        });
-
-        for (let key in proxy) {
-          if (typeof proxy[key] === "function") {
-            proxy[key] = proxy[key].bind(newResponse);
-          }
-        }
-
-        return proxy;
-      } else {
-        return response;
-      }
-    });
-  }
+    }
 };
 
 window.addEventListener(
-  "message",
-  function(event) {
-    const data = event.data;
-    if (data.type === "ajaxInterceptor" && data.to === "pageScript") {
-      ajax_interceptor_qoweifjqon.settings[data.key] = data.value;
+    'message',
+    function(event) {
+        const data = event.data;
+        if (data.type === 'ajaxInterceptor' && data.to === 'pageScript') {
+            ajax_interceptor_qoweifjqon.settings[data.key] = data.value;
+        }
+
+        if (ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_switchOn) {
+            window.XMLHttpRequest = ajax_interceptor_qoweifjqon.myXHR;
+            window.fetch = ajax_interceptor_qoweifjqon.myFetch;
+        } else {
+            window.XMLHttpRequest = ajax_interceptor_qoweifjqon.originalXHR;
+            window.fetch = ajax_interceptor_qoweifjqon.originalFetch;
+        }
+    },
+    false
+);
+
+// 处理新消息
+function handleNewMessage(responseJSON,version){
+    if(version === '2.0'){
+        try {
+            if (!responseJSON.data.list || responseJSON.data.list.length === 0) {
+                return;
+            }
+            let line_id = responseJSON.data.list[0].item.info.line_id;
+            let name = responseJSON.data.list[0].item.name;
+            let timeout = null;
+            if (name === 'message_new') {
+                timeout = setTimeout(function() {
+                    console.log(line_id);
+                    replyMessage2(line_id,version);
+                }, window.replyTime);
+            }
+
+            // 构建一个回复用户的timeout列表。
+            if(userReplyMap[line_id]){
+                timeout && (userReplyMap[line_id].push(timeout));
+            } else {
+                timeout && (userReplyMap[line_id] = [timeout]);
+            }
+        } catch (e) {}
+    }
+}
+
+// 检查回复列表中的数据
+function checkReplyList(line_id){
+    let replyList = window.userReplyMap[line_id];
+    if(!replyList){
+        return;
     }
 
-    if (ajax_interceptor_qoweifjqon.settings.ajaxInterceptor_switchOn) {
-      window.XMLHttpRequest = ajax_interceptor_qoweifjqon.myXHR;
-      window.fetch = ajax_interceptor_qoweifjqon.myFetch;
-    } else {
-      window.XMLHttpRequest = ajax_interceptor_qoweifjqon.originalXHR;
-      window.fetch = ajax_interceptor_qoweifjqon.originalFetch;
+    // 如果还有排队的，请删除
+    replyList.forEach(function(item){
+        clearTimeout(item);
+    })
+    window.userReplyMap[line_id] = [];
+}
+
+// 回复用户
+function replyMessage2(line_id,version){
+    checkReplyList(line_id);
+
+    let url = '';
+    if(version === '2.0'){
+        url = POST_MESSAGE_URLS[0].match
     }
-  },
-  false
-);
+
+    $.ajax({
+        url: url,
+        method: 'post',
+        data: {
+            line_id: line_id,
+            message: {
+                type: 1,
+                content: {
+                    text: window.replyText
+                }
+            }
+        },
+        success: function(res) {
+            console.log(res);
+        },
+        error: function() {}
+    });
+}
+
+
+// $.ajax({
+//   url: "https://im.mafengwo.cn/rest/im/event/",
+//   method: "post",
+//   data: {
+//     update: {
+//       e: "req.message.post",
+//       b: {
+//         message: {
+//           type: 1,
+//           to_uid: "484173610",
+//           timestamp: new Date().getTime(),
+//           content: {
+//             text: "您好！"
+//           },
+//           prefix_id: new Date().getTime()
+//         },
+//         role: {
+//           is_b: "1",
+//           is_pc: "1"
+//         },
+//         t: new Date().getTime(),
+//         v: "1.0"
+//       }
+//     },
+//     post_style: "default",
+//     after_style: "default"
+//   },
+//   success: function(res) {
+//     console.log(res);
+//   },
+//   error: function() {}
+// });
